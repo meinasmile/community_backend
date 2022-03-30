60f6c9dd070beb34e0921e81839fda86ee1fa314
meinasmile <meinasmile@gmail.com>
2022-03-30 09:32:52 -0700

    user register

--- src/main/java/com/projects/doubao/mapper/UmsUserMapper.java
+++ src/main/java/com/projects/doubao/mapper/UmsUserMapper.java
@@ -1,10 +1,10 @@
 package com.projects.doubao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.projects.doubao.model.entity.BmsTip;
+import com.projects.doubao.model.entity.UmsUser;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BmsTipMapper extends BaseMapper<BmsTip> {
-    BmsTip getRandomTip();
+public interface UmsUserMapper extends BaseMapper<UmsUser> {
+
 }
---
+++ src/main/java/com/projects/doubao/service/IUmsUserService.java
@@ -0,0 +1,16 @@
+package com.projects.doubao.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.projects.doubao.model.dto.RegisterDTO;
+import com.projects.doubao.model.entity.UmsUser;
+
+public interface IUmsUserService extends IService<UmsUser> {
+    /**
+     * register function
+     *
+     * @param dto
+     * @return register object
+     */
+
+    UmsUser executeRegister(RegisterDTO dto);
+}
